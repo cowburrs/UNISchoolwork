5e01dad27231b7cfb8b80a4a1f0564eb2b1afa7f
Your Name <you@example.com>
2026-04-12 21:33:51 +1000

    gitshit

--- 6week/Programming/chars.lua
+++ 6week/Programming/chars.lua
@@ -22,29 +22,28 @@ function dump(o)
 	end
 end
 
--- local function summation(x)
--- 	if x <= 1 then
--- 		return 11
--- 	else
--- 		local m = x - 1
--- 		return summation(x - 1) + 17 * m ^ 2 + 22 * m
--- 	end
--- end
--- for i = 1, 5, 1 do
--- 	print(summation(i))
--- end
+local function summation(x)
+	if x <= 1 then
+		return 11
+	else
+		local m = x - 1
+		return summation(x - 1) + 17 * m ^ 2 + 22
+	end
+end
+for i = 1, 5, 1 do
+	print(summation(i))
+end
 function summation(n)
-	return 11 + 17 * (n - 1) * n * (2 * n - 1) / 6 + 22 * (n - 1) * n / 2
+	return 11 + 17 * (n - 1) * n * (2 * n - 1) / 6 + 22 * (n - 1)
 end
 for i = 1, 5, 1 do
 	print(summation(i))
 end
 local somestring = ""
-for i = 0, 9, 1 do
-    somestring = somestring .. " + 17*" .. i .. "^2"
-    local n = i + 1
-    local endcoefficient = ((n - 1) * n / 2)
-    local printstring = "11 + 22*" .. endcoefficient .. somestring
-    print(load("return " .. printstring)())
-	 print(printstring)
+for i = 0, 4, 1 do
+	somestring = somestring .. " + 17*" .. i .. "^2"
+	local endcoefficient = i
+	local printstring = "11 + 22*" .. endcoefficient .. somestring
+	print(load("return " .. printstring)())
+	-- print(printstring)
 end
