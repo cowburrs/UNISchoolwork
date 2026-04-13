86b7ef8f3c2e16b04d94e888be12e13987e355a6
Your Name <you@example.com>
2026-04-13 21:28:38 +1000

    gitshit

--- flake.nix
+++ flake.nix
@@ -19,6 +19,10 @@
           packages = [
             vpython.packages.x86_64-linux.vpython
           ];
+          shellHook = ''
+            echo "vpython enabled"
+            export PS1="\[\033[0;32m\]vpython-\e[38;5;141m\]\$\[\e[0m\]\[\033[0m\] "
+          '';
         };
     };
 }
