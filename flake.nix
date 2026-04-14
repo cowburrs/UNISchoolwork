780cd752596630ef2ca257b69580d648b69cecb7
Your Name <you@example.com>
2026-04-14 14:32:56 +1000

    gitshit

--- flake.nix
+++ flake.nix
@@ -10,10 +10,27 @@
       vpython,
       ...
     }:
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in
     {
-      devShells.x86_64-linux.default = nixpkgs.legacyPackages.x86_64-linux.mkShell {
+      devShells.x86_64-linux.default = pkgs.mkShell {
         packages = [
           vpython.packages.x86_64-linux.vpython
+
+          (pkgs.python3.withPackages (
+            python-pkgs: with python-pkgs; [
+              pyserial
+              holidays
+              pyqt6
+              uncertainties
+              pint
+              pandas
+              matplotlib
+              numpy
+              odfpy
+            ]
+          ))
         ];
         shellHook = ''
           export PS1="\n\[\033[1;32m\][nix-shell:\w]\$\[\033[0m\] "
